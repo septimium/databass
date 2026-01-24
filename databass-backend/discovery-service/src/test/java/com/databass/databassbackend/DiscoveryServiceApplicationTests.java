bf4305612132d54c30622c7a4465cf2f4bc98b67
septimium <septimiutg@gmail.com>
2026-01-24 15:08:51 +0200

    [1] Created Discovery Service (Eurika).

---
+++ databass-backend/discovery-service/src/test/java/com/databass/databassbackend/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.databass.databassbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
